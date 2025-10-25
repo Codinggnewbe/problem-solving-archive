3e787d80a1c5293c7732d7e113a833dcbef50017
Kim Minsu <166682803+Codinggnewbe@users.noreply.github.com>
2025-10-26 02:00:25 +0900

    [Silver IV] Title: 바닥 장식, Time: 108 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    final static int MAX =  50 + 10;
+    static char[][] map;
+    static boolean[][] visited;
+    static int N, M;
+
+    public static void dfs(int y, int x){
+        visited[y][x] = true;
+
+        if(map[y][x] == '-' && map[y][x + 1] == '-') dfs(y, x+1);
+        if(map[y][x] == '|' && map[y + 1][x] == '|') dfs(y+1, x);
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new char[MAX][MAX];
+        visited = new boolean[MAX][MAX];
+
+        // map
+        for(int i = 1; i <= N; i++){
+            String str = br.readLine();
+            for(int j = 1; j <= M; j++){
+                map[i][j] = str.charAt(j - 1); // 1부터 시작하니까 1을 뺌
+            }
+        }
+
+        // dfs
+        int answer = 0;
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= M; j++){
+                if(visited[i][j] == false){
+                    dfs(i, j);
+                    answer++;
+                }
+            }
+        }
+
+        // 출력
+        bw.write(String.valueOf(answer));
+
+        br.close(); bw.close();
+    }
+}
