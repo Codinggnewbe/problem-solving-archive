2340bb3de2239983266e0d597701996c4b846ff7
KIM MINSU <166682803+Codinggnewbe@users.noreply.github.com>
2026-05-12 12:06:06 +0900

    [level 0] Title: 조건 문자열, Time: 0.01 ms, Memory: 8.97 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -0,0 +1,13 @@
+def solution(ineq, eq, n, m):
+    answer = 0
+    if(eq == "!"):
+        if(ineq == ">" and n > m or ineq == "<" and n < m):
+            answer = 1
+        else:
+            answer = 0
+    else:
+        if(ineq == ">" and n >= m or ineq == "<" and n <= m):
+            answer = 1
+        else:
+            answer = 0
+    return answer
