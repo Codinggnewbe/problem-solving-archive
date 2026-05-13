c395b501c1551c5aa6230784d0c0ded551f69b1c
KIM MINSU <166682803+Codinggnewbe@users.noreply.github.com>
2026-05-14 00:01:34 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -0,0 +1,12 @@
+def solution(num_list):
+    answer = []
+    leng = len(num_list)
+    
+    for i in range(leng):
+            answer.append(num_list[i])
+            
+    if(num_list[leng - 2] < num_list[leng - 1]):
+        answer.append(num_list[leng - 1] - num_list[leng - 2])
+    else:
+        answer.append(num_list[leng - 1] * 2)
+    return answer
